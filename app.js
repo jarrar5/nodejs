cadf0e3c3264d697ea776057fac157d6c4bba561
jarrar5 <jarrar5@hotmail.com>
2020-12-17 11:29:52 +0000

    setting up environment and basic views

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const message = 'Hello World';
+const express = require('express');
+const path = require('path');
 
-console.log(message, 'from Gitpod!');
+const app =express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function(){
+    console.log('Server listening on port 3000. . . . .');
+});
+
+app.get('/', (req, res) => {
+  res.render('index',{
+    title: 'Home'
+  });
+})
+
+app.get('/books', function (req, res) {
+  console.log('books.html!');
+  res.render('books',{
+    title: 'Books'
+  });
+});
